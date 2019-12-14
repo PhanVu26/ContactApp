bb0f22970943408482bf8609ac99d1d174f71572
PhanVu <phanvu200470@gmail.com>
2019-12-14 21:16:18 +0700

    Bỏ chức năng đồng bộ danh bạ

--- app/src/main/java/com/example/contact/MainActivity.java
+++ app/src/main/java/com/example/contact/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_contacts, R.id.navigation_recents, R.id.navigation_favourites,R.id.navigation_synchronize)
+                R.id.navigation_contacts, R.id.navigation_recents, R.id.navigation_favourites)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
